0bff76717c1536876251f2a7c66fc5cb07f8e8c9
henriquesourcecodes83 <127987495+henriquesourcecodes83@users.noreply.github.com>
2023-03-21 20:49:38 -0300

    Update javaarrayexercises.js

--- javaarrayexercises.js
+++ javaarrayexercises.js
@@ -86,3 +86,95 @@ function insertDashesIntoEven(number){
 
     return null;
 }
+
+
+// 8. Write a JavaScript program to find the most frequent item of an array
+// Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];// Sample Output : a ( 5 times )
+
+function findMostFrequent(array){
+    
+    var mostfrequencyItem;
+    var frequency = 0;
+    var mostfrequency = 0;
+
+    for(var i = 0; i < array.length; i++){        
+
+        for(var j = i; j < array.length; j++){
+            if(array[i] == array[j]){
+                frequency++;
+            }
+        }
+
+        if(frequency > mostfrequency){
+            mostfrequencyItem = array[i];
+            mostfrequency = frequency;
+        }
+
+        frequency = 0;    
+    }
+
+    return mostfrequencyItem;
+}
+
+// 9. Write a JavaScript program which accept a string as input and swap the case of 
+// each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX
+function swapCase(string){
+
+    var arrayString = String(string).split("");
+
+    for(var i = 0; i < arrayString.length; i++){       
+
+        if(arrayString[i].toLowerCase() == arrayString[i]){
+            arrayString[i] = arrayString[i].toUpperCase();
+        }
+        else{
+            arrayString[i] = arrayString[i].toLocaleLowerCase()
+        }
+    }
+    
+    return(arrayString.join(""));
+
+}
+
+// 12. Escreva um programa JavaScript para calcular a soma e o produto de um array de inteiros
+function arraySomaEMultiplicacao(array){
+    var soma = 0;
+    var multiplicacao = 1;
+
+    for(var i = 0; i < array.length; i++){
+        soma += array[i];
+        multiplicacao *= array[i];
+    }
+
+    return "Soma = " + soma + " e multiplicacao = " + multiplicacao;
+}
+
+// 13. Escreva um programa JavaScript para adicionar itens em um array em branco e exibir os itens
+
+// <form>
+//  <input type="text" id="element_id" name="element">
+//  <input type="button" name="btnAdd" value="add" onclick="doAdd();">
+//  <input type="button" name="btnDisplay" value="display" onclick="doDisplay();">
+// </form>
+// <p id="display_area"></p>
+
+var x = 0;
+var addedElements = new Array();
+
+function doAdd(){
+    addedElements[x] = document.getElementById("element_id").value;
+    x++;
+    document.getElementById("element_id").value = "";
+}
+
+function doDisplay(){
+
+    var result = "";
+
+    for(var i = 0; i < x; i++) {
+        result += addedElements[i] + "<br />";
+    }
+    
+    document.getElementById("display_area").innerHTML = result;
+
+}
